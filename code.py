daf7a195dce0d472ade85d498817d687f862bad8
monikasafhauzer <144321086+monikasafhauzer@users.noreply.github.com>
2023-10-20 14:38:50 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,10 @@
+# Import needed libraries
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import csv
 
-# Define the data processing functions
+# Define the data by number of spots in three groups (low,mid and high) 
 def categorize_spots(num_spots):
     if num_spots <= 5:
         return 'low'
@@ -11,7 +12,7 @@ def categorize_spots(num_spots):
         return 'mid'
     else:
         return 'high'
-
+# Define the data by spot area into 5 groups of varying sizes
 def categorize_area(spot_area):
     if 0 <= spot_area < 0.1:
         return '0-0.1'
@@ -25,19 +26,19 @@ def categorize_area(spot_area):
         return '0.4-0.5'
     else:
         return '0.5+'
-
+        
+# Define how to process your data, define and create an input and output file, name those
 def process_data(input_file, output_file):
     df = pd.read_csv(input_file)
     df['Spot Category'] = df['num_spots'].apply(categorize_spots)
     df['Area Category'] = df['spot_area'].apply(categorize_area)
     df.to_csv(output_file, index=False)
-
-# Process the data
+    
 input_file = 'test_data.csv'
 output_file = 'output.csv'
 process_data(input_file, output_file)
 
-# Load the processed data from the output file
+# Load the processed data from the output file, this step is so you can use your output info for plotting
 data = pd.read_csv('output.csv')
 
 # Sort data for Cells A and B by num_spots
@@ -46,10 +47,8 @@ cell_b_data = data[data['Cell_Type'] == 'Cell_B'].sort_values('num_spots')
 
 # Create a bar plot for num_spots
 plt.figure(figsize=(10, 6))
-
 plt.bar(cell_a_data['Cell_Type'], cell_a_data['num_spots'], label='Cell A')
 plt.bar(cell_b_data['Cell_Type'], cell_b_data['num_spots'], label='Cell B')
-
 plt.xlabel('Cell')
 plt.ylabel('num_spots')
 plt.title('Comparison of Cells A and B by num_spots')
@@ -60,16 +59,17 @@ plt.show()
 cell_a_data = data[data['Cell_Type'] == 'Cell_A'].sort_values('spot_area')
 cell_b_data = data[data['Cell_Type'] == 'Cell_B'].sort_values('spot_area')
 
-# Create a bar plot for spot_area
+# Create a new figure for the second plot
 plt.figure(figsize=(10, 6))
 
+# Create a bar plot for spot_area
 plt.bar(cell_a_data['Cell'], cell_a_data['spot_area'], label='Cell A')
 plt.bar(cell_b_data['Cell'], cell_b_data['spot_area'], label='Cell B')
-
 plt.xlabel('Cell')
 plt.ylabel('spot_area')
 plt.title('Comparison of Cells A and B by spot_area')
 plt.legend()
 plt.show()
 
-fig.savefig(“pbody.png”)
+# Save your figure as a png
+plt.savefig("pbody.png")
